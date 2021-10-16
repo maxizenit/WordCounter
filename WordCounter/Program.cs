7a489c4acad0c64be757de6f870f84a592d47da6
Maxim Puzanov <maxizenit@gmail.com>
2021-10-16 05:01:49 +0300

    Fixed output

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -10,18 +10,18 @@ namespace WordCounter
             Console.Write("Введите путь к обрабатываемому файлу: ");
             wb.ReadFile(Console.ReadLine());
             Console.Write("Вывести результат в консоли? (y/n): ");
-            if (Console.ReadKey().Key == ConsoleKey.Y)
+            if (Char.ToLower(Console.ReadLine()[0]) == 'y')
             {
                 wb.Print();
             }
-            Console.Write("\nВывести результат в файл? (y/n): ");
-            if (Console.ReadKey().Key == ConsoleKey.Y)
+            Console.Write("Вывести результат в файл? (y/n): ");
+            if (Char.ToLower(Console.ReadLine()[0]) == 'y')
             {
-                Console.Write("\nВведите путь к файлу с файлу с результатом: ");
+                Console.Write("Введите путь к файлу с файлу с результатом: ");
                 wb.WriteInFile(Console.ReadLine());
             }
-            Console.Write("\nНажмите любую клавишу: ");
-            Console.ReadKey();
+            Console.Write("Нажмите Enter для выхода: ");
+            Console.ReadLine();
         }
     }
 }
